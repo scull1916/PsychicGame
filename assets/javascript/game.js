e257739cc5e3f2a3c2e767b5947c25652741fbc4
scull1916 <kpscully@gmail.com>
2018-01-02 20:11:31 -0500

    revised to place js in a separate script file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,40 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
-var guesses = 10;
-var won = 0;
-var lost = 0;
-var computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
-var guess = [];
-console.log(computerPick);
+		var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+		var guesses = 10;
+		var won = 0;
+		var lost = 0;
+		var computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
+		var guess = '';
+		console.log(computerPick);
 
-for (i=0; i<10; i++) 
-{
-	var lettersGuess = document.getElementById("letter-text");
-	document.onkeyup = function(event) 
-	{
-		lettersGuess.textContent = (event.key);
+			
+		for (i=0; i<10; i++) 
+		{
+			
+			var lettersGuess = document.getElementById("letter-text");
+			document.onkeyup = function(event) 
+			{
+			lettersGuess.textContent = event.key;
 
-	}
+				}
 
 
-}
+
+				if (lettersGuess === computerPick) {
+					won ++;
+					
+				}
+
+				else {
+					lost ++;
+					guesses --;
+					
+					
+			}
+
+			
+
+		};
+				document.getElementById('letter-text').innerHTML = guess;
+				document.getElementById('loses').innerHTML = lost;
+				document.getElementById('left').innerHTML = guesses;
+				document.getElementById('wins').innerHTML = won;
